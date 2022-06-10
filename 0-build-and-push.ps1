39bcaba896703b6ca5160db10cd5c263d71d9604
Petr Gašparík <petr@gasparik.cz>
2022-06-11 00:05:12 +0200

    Update 220611-0001

--- 0-build-and-push.ps1
+++ 0-build-and-push.ps1
@@ -1,5 +1,6 @@
 flutter build web --base-href "/next_cc/"
 Copy-Item ".\build\web\*" ".\docs\" -Force -Recurse 
 $datetime = Get-Date -Format "yyMMdd-HHmm"  
+git add --all
 git commit -m "Update $datetime"
 git push
